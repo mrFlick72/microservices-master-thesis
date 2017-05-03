5a8a1be1b11b7e84f143419a77f12aa12976f6d2
Valerio Vaudi <valerio.vaudi@sourcesense.com>
2017-05-03 11:46:02 +0200

    configuration-server first commit

--- configuration-server/src/main/java/it/valeriovaudi/emarket/ConfigurationServerApplication.java
+++ configuration-server/src/main/java/it/valeriovaudi/emarket/ConfigurationServerApplication.java
@@ -2,13 +2,15 @@ package it.valeriovaudi.emarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+@EnableConfigServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class ConfigurationServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(ConfigurationServerApplication.class, args);
 	}
 }
