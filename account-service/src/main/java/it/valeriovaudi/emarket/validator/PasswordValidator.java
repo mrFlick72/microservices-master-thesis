ab67177c2add992ed89f841aefae540f1b6663ea
Valerio Vaudi <valerio.vaudi@sourcesense.com>
2017-05-03 20:14:51 +0200

    validator annotation

--- account-service/src/main/java/it/valeriovaudi/emarket/validator/PasswordValidator.java
+++ account-service/src/main/java/it/valeriovaudi/emarket/validator/PasswordValidator.java
@@ -3,10 +3,14 @@ package it.valeriovaudi.emarket.validator;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.NotNull;
+import java.lang.annotation.*;
 
 /**
  * Created by mrflick72 on 03/05/17.
  */
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 @NotNull
 @NotEmpty
 public @interface PasswordValidator {
--- account-service/src/main/java/it/valeriovaudi/emarket/validator/TaxCode.java
+++ account-service/src/main/java/it/valeriovaudi/emarket/validator/TaxCode.java
@@ -4,11 +4,15 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.lang.annotation.*;
 
 /**
  * Created by mrflick72 on 03/05/17.
  */
 
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 @NotNull
 @NotEmpty
 @Size(min = 16, max = 16)
