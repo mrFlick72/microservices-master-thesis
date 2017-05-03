5a3128d297b63446acc1171b671de00306e4bc71
Valerio Vaudi <valerio.vaudi@sourcesense.com>
2017-05-03 20:28:32 +0200

    view conrtroller login page, docker file

--- account-service/src/main/java/it/valeriovaudi/emarket/web/WebConfig.java
+++ account-service/src/main/java/it/valeriovaudi/emarket/web/WebConfig.java
@@ -1,6 +1,7 @@
 package it.valeriovaudi.emarket.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -9,4 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
 }
