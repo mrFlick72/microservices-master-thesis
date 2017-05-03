64aeaa37bfdef2c1a9e9bf96789f98109036f8b3
Valerio Vaudi <valerio.vaudi@sourcesense.com>
2017-05-03 20:21:20 +0200

    login sso page, web configuration, security configuration, Account endpoint , doker file

---
+++ account-service/src/main/java/it/valeriovaudi/emarket/endpoint/restfull/AccountRestFullEndPoint.java
@@ -0,0 +1,13 @@
+package it.valeriovaudi.emarket.endpoint.restfull;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by mrflick72 on 03/05/17.
+ */
+
+@RestController
+@RequestMapping("/account")
+public class AccountRestFullEndPoint {
+}
---
+++ account-service/src/main/java/it/valeriovaudi/emarket/web/WebConfig.java
@@ -0,0 +1,12 @@
+package it.valeriovaudi.emarket.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by mrflick72 on 03/05/17.
+ */
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
