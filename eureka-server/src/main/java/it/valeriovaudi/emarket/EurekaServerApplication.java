8ab11e5dfc936242c630595beab972fddc67a5ff
Valerio Vaudi <valerio.vaudi@sourcesense.com>
2017-05-04 15:16:28 +0200

    refactor

--- eureka-server/src/main/java/it/valeriovaudi/emarket/EurekaServerApplication.java
+++ eureka-server/src/main/java/it/valeriovaudi/emarket/EurekaServerApplication.java
@@ -3,8 +3,9 @@ package it.valeriovaudi.emarket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
