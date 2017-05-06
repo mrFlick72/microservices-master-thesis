6544f4b77998ba330da18b1019300fab0cdf4985
mrFlick72 <Back72Star>
2017-05-06 20:05:50 +0200

    configuration infrastructural pieces

--- turbine-server/src/main/java/it/valeriovaudi/emarket/TurbineServerApplication.java
+++ turbine-server/src/main/java/it/valeriovaudi/emarket/TurbineServerApplication.java
@@ -2,9 +2,11 @@ package it.valeriovaudi.emarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
-
+@EnableEurekaClient
 @EnableTurbineStream
 @SpringBootApplication
 public class TurbineServerApplication {
