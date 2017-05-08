0a39775c328c205b31baaca0d3fe53949885f85d
Valerio Vaudi <valerio.vaudi@sourcesense.com>
2017-05-08 17:14:01 +0200

    autentication routing

--- api-gateway/src/main/java/it/valeriovaudi/emarket/ApiGatewayApplication.java
+++ api-gateway/src/main/java/it/valeriovaudi/emarket/ApiGatewayApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableHystrix
-//@EnableOAuth2Sso
 @EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -14,29 +13,3 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 }
-
-/*
-@Configuration
-class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-	@Override
-	public void init(WebSecurity web) {
-		web.ignoring().antMatchers("/");
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().antMatcher("*/
-/**").authorizeRequests().anyRequest().authenticated();
-	}
-
-}
-
-class WebConfig extends WebMvcConfigurerAdapter {
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/login").setViewName("login");
-	}
-}
-*/
