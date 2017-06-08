e3ec3cfa6d4a8b82f0b8408cbe7814aced4355bc
Valerio Vaudi <valerio.vaudi@sourcesense.com>
2017-06-08 19:41:38 +0200

    exception

---
+++ purchase-order-service/src/main/java/it/valeriovaudi/emarket/exception/PurchaseOrderNotFoundException.java
@@ -0,0 +1,21 @@
+package it.valeriovaudi.emarket.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by vvaudi on 12/05/17.
+ */
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class PurchaseOrderNotFoundException extends RuntimeException {
+
+    public static final String DEFAULT_MESSAGE = "purchase order didn't found";
+    public PurchaseOrderNotFoundException(String msg) {
+        super( msg);
+    }
+
+    public PurchaseOrderNotFoundException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
