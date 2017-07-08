a7bf5a5bc1e8de90490d06535910db106a8750e0
mrFlick72 <Back72Star>
2017-07-08 19:42:10 +0200

    exception logging in fallback

--- purchase-order-service/src/main/java/it/valeriovaudi/emarket/integration/AccountIntegrationService.java
+++ purchase-order-service/src/main/java/it/valeriovaudi/emarket/integration/AccountIntegrationService.java
@@ -5,6 +5,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import it.valeriovaudi.emarket.anticorruptation.account.AccountAnticorruptationLayerService;
 import it.valeriovaudi.emarket.model.Customer;
 import it.valeriovaudi.emarket.model.CustomerContact;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +14,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.net.URLEncoder;
 
 /**
  * Created by mrflick72 on 30/05/17.
  */
 
+@Slf4j
 @Service
 public class AccountIntegrationService extends AbstractIntegrationService {
 
@@ -54,11 +57,17 @@ public class AccountIntegrationService extends AbstractIntegrationService {
     /**
      * fallback
      * */
-    public CustomerContact getCustomerContactFormAccountDataFallback(String userName) {
+    public CustomerContact getCustomerContactFormAccountDataFallback(String userName, Throwable e) {
+        log.error(String.format("fire fallback getCustomerContactFormAccountDataFallback for user: %s", userName));
+        log.error(String.format("Exception message: %s", e.getMessage()));
+        e.printStackTrace();
         return null;
     }
 
-    public Customer getCustomerFormAccountDataFallback(String userName) {
+    public Customer getCustomerFormAccountDataFallback(String userName, Throwable e) {
+        log.error(String.format("fire fallback getCustomerFormAccountDataFallback for user: %s", userName));
+        log.error(String.format("Exception message: %s", e.getMessage()));
+        e.printStackTrace();
         return null;
     }
 }
