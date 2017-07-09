f0acf253bd5c333226139b683d0e16357dcb998a
Valerio Vaudi <valerio.vaudi@gmail.com>
2017-07-09 17:34:18 +0200

    add enable eureka client

--- api-gateway/src/main/java/it/valeriovaudi/emarket/ApiGatewayApplication.java
+++ api-gateway/src/main/java/it/valeriovaudi/emarket/ApiGatewayApplication.java
@@ -2,11 +2,13 @@ package it.valeriovaudi.emarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableHystrix
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
