28ec990de9fc524d37d2cd3d59ada16873a11355
mrFlick72 <Back72Star>
2017-09-30 16:07:03 +0200

    last commit

--- account-service/src/main/java/it/valeriovaudi/emarket/repository/AccountRepository.java
+++ account-service/src/main/java/it/valeriovaudi/emarket/repository/AccountRepository.java
@@ -8,23 +8,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Created by mrflick72 on 03/05/17.
  */
-public interface AccountRepository extends JpaRepository<Account,String> {
-
-    @Override
-    @CachePut(value = "account")
-    Account save(Account entity);
-
-
-    @Override
-    @CachePut(value = "account")
-    void delete(String userName);
-
-    @Override
-    @CachePut(value = "account")
-    void delete(Account account);
-
-
-    @Override
-    @Cacheable(value = "account")
-    Account findOne(String userName);
-}
+public interface AccountRepository extends JpaRepository<Account,String> { }
